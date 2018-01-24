a22af976b87a7707ac4ca2ae9273955d14889b6f
Jakub Mitulski <jakub.mitulski@gmail.com>
2018-01-24 23:44:19 +0100

    Update

--- src/Library/Main.java
+++ src/Library/Main.java
@@ -10,32 +10,26 @@ public class Main {
         boolean canContinue = true;
         Scanner userInput = new Scanner(System.in);
         List<Book> dataBase = new ArrayList<>();
-
         do {
-            System.out.println("Choose the option: 1 - new item; 2 - remove item; 3 - search item; 4 - print list; 5 - change status; 6 - exit");
-            int number = userInput.nextInt();
+            System.out.println("\n Choose the option: 1 - new item; 2 - remove item; 3 - search item; 4 - print list; 5 - change status; 6 - exit");
 
+            int number = userInput.nextInt();
             switch (number) {
                 case 1:
                     addItem(dataBase);
                     break;
-
                 case 2:
                     removeItem(dataBase);
                     break;
-
                 case 3:
                     searchItem(dataBase);
                     break;
-
                 case 4:
                     printList(dataBase);
                     break;
-
                 case 5:
                     changeStatus(dataBase);
                     break;
-
                 case 6:
                     canContinue = false;
                     System.out.println("See you next time!");
@@ -44,41 +38,56 @@ public class Main {
         } while (canContinue);
     }
 
+
+    public static void printWarning() {
+        System.out.println("Sorry, but your database is empty - add some book first");
+    }
+
     public static Book searchItem(List<Book> list) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type a title: ");
-        String searchedTitle = sc.nextLine();
         Book result = null;
 
-        for (int i = 0; i < list.size(); i++) {
-            String title = list.get(i).getTitle();
-            if (title.equals(searchedTitle)) {
-                result = list.get(i);
-                System.out.println(result);
-            } else {
-                System.out.println("Sorry, cannot find item: " + searchedTitle);
+        if (!list.isEmpty()) {
+            System.out.println("Type a title: ");
+            String searchedTitle = sc.nextLine();
+            for (int i = 0; i < list.size(); i++) {
+                String title = list.get(i).getTitle();
+                if (title.equals(searchedTitle)) {
+                    result = list.get(i);
+                    System.out.println(result);
+                } else {
+                    System.out.println("Sorry, cannot find item: " + searchedTitle);
+                }
             }
+        } else {
+            printWarning();
         }
         return result;
     }
 
     public static void removeItem(List<Book> list) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type a title to remove: ");
-        String searchTitle = sc.nextLine();
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getTitle().equals(searchTitle)) {
-                System.out.println("Item: " + list.get(i) + " - has been removed");
-                list.remove(i);
+        if (!list.isEmpty()) {
+            System.out.println("Type a title to remove: ");
+            String searchTitle = sc.nextLine();
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i).getTitle().equals(searchTitle)) {
+                    System.out.println("Item: " + list.get(i) + " - has been removed");
+                    list.remove(i);
+                }
             }
+        } else {
+            printWarning();
         }
     }
 
     public static List printList(List<Book> list) {
-        System.out.println("Here is your list: ");
-        for (Object index : list) {
-            System.out.println(index);
+        if (!list.isEmpty()) {
+            System.out.println("Here is your list: ");
+            for (Object index : list) {
+                System.out.println(index);
+            }
+        } else {
         }
         return list;
     }
@@ -86,11 +95,11 @@ public class Main {
     public static void addItem(List<Book> list) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Type title: ");
+        System.out.print("Type book's title: ");
         String title = sc.nextLine();
-        System.out.print("Type first name: ");
+        System.out.print("Type author's first name: ");
         String firstName = sc.nextLine();
-        System.out.print("Type last name: ");
+        System.out.print("Type author's last name: ");
         String lastName = sc.nextLine();
         System.out.print("Type amount of items: ");
         Integer amount = sc.nextInt();
@@ -102,28 +111,28 @@ public class Main {
 
     public static void changeStatus(List<Book> list) {
         try {
-        Book book = searchItem(list);
-        Integer amount = book.getAmount();
-        System.out.println("To hire type 1, to return type 2: ");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        switch (number) {
-            case 1:
-                amount--;
-                break;
-            case 2:
-                amount++;
-                break;
-        }
+            Book book = searchItem(list);
+            Integer amount = book.getAmount();
+            System.out.println("To hire type 1, to return type 2: ");
+            Scanner sc = new Scanner(System.in);
+            int number = sc.nextInt();
+            switch (number) {
+                case 1:
+                    amount--;
+                    break;
+                case 2:
+                    amount++;
+                    break;
+            }
             if (amount >= 0) {
                 book.setAmount(amount);
             } else {
                 System.out.println("Sorry, item not available");
             }
             System.out.println(book);
-        } catch (java.lang.NullPointerException e){
-            System.out.println("Database empty. Add item first.");
+        } catch (java.lang.NullPointerException e) {
         }
     }
+    
 
 }
